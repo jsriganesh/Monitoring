3d03ad150ba9430cb236a84559a39b5fa85d9463
jsriganesh <jsriganesh535@gmail.com>
2024-03-16 20:57:30 +0530

    add chart js

--- monitoring-fe-app/src/pages/dashboard/index.tsx
+++ monitoring-fe-app/src/pages/dashboard/index.tsx
@@ -2,6 +2,70 @@ import Header from "../../components/header";
 import "./dashboardStyle.scss";
 import { useNavigate } from "react-router-dom";
 
+
+import { Pie } from '@ant-design/plots';
+interface DataType {
+  type: string;
+  value: number;
+}
+
+const DemoPie = () => {
+
+  const colors: Record<string, string>  = {
+    "Low battery": 'red',
+    "In Charge": 'blue',
+    "Above 50%": 'green',
+  };
+
+  const config = {
+    data: [
+      { type: "Above50%", value: 10 },
+      { type: "Low battery", value: 50 },
+      { type: "In Charge", value: 40 },
+      { type: "In Charge", value: 40 },
+      { type: "In Charge", value: 40 },
+      { type: "In Charge", value: 40 },
+    ] as DataType[] ,
+    // color: (type:string) => colors[type],
+    color: ['type', (type:string) => colors[type]],
+
+    angleField: 'value',
+    colorField: 'type',
+    paddingRight: 100,
+    innerRadius: 0.7,
+    // radius: 0.8,
+    label: {
+      text: 'value',
+      style: {
+        fontWeight: 'bold',
+      },
+    },
+    legend: {
+      color: {
+        // title: false,
+        position: 'right',
+        rowPadding: 5,
+      },
+    },
+    annotations: [
+      {
+        type: 'text',
+        style: {
+          text: '20',
+          x: '50%',
+          y: '50%',
+          textAlign: 'center',
+          fontSize: 18,
+          fontStyle: 'bold',
+        },
+      },
+    ],
+  };
+  return <Pie {...config} />;
+};
+
+
+
 const Dashboard = () => {
   const navigate = useNavigate();
 
@@ -25,7 +89,7 @@ const Dashboard = () => {
 
         <div className="dashboard-chart-details">
           <div className="battery-status-card">
-
+            <DemoPie/>
           </div>
           <div className="battery-status-card">
 
